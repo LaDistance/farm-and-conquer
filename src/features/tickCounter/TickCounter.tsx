ce3fc3ed8ede62564983740f1ab88374dffdbfe5
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-01 20:26:42 +0200

    Counter

--- src/features/tickCounter/TickCounter.tsx
+++ src/features/tickCounter/TickCounter.tsx
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import {
-  increment,
-  selectTickCount,
-} from './tickCounterSlice';
-import styles from './TickCounter.module.scss';
-import { useTimer } from 'react-timer-hook';
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { increment, selectTickCount } from "./tickCounterSlice";
+import styles from "./TickCounter.module.scss";
+import { useTimer } from "react-timer-hook";
+import { Card } from "antd";
 
-export const TickCounter = ({ expiryTimestamp } : { expiryTimestamp: Date}) => {
-  const tickCount = useAppSelector(selectTickCount);
-  const dispatch = useAppDispatch();
-  const [flipState, setFlipState] = useState(true);
-
-  useEffect(() => {
-      const time = new Date();
-      time.setSeconds(time.getSeconds() + 10);
-      restart(time);
-  }, [flipState])
-
-  const {
-      seconds,
-      restart,
-  } = useTimer({ expiryTimestamp, onExpire: () => { 
-    setFlipState(!flipState)
-    dispatch(increment()); 
-  }
-}
-);
-
-
+export const TickCounter = ({
+  tickCount,
+  seconds,
+}: {
+  tickCount: number;
+  seconds: number;
+}) => {
   return (
-    <div className={styles.row}>
-    <span className={styles.value}>{seconds}</span>
-    <span className={styles.value}>{tickCount}</span>
-  </div>
+    <Card title="Card title">
+      <div className={styles.row}>
+        <span className={styles.value}>{seconds}</span>
+        <span className={styles.value}>{tickCount}</span>
+      </div>
+    </Card>
   );
-}
+};
