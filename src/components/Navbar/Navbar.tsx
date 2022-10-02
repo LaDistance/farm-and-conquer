776ea91f4488cbc20a7cc8237633a66089beb1a4
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-02 15:22:29 +0200

    Updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,10 @@
-import { $CombinedState } from "@reduxjs/toolkit";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 import { selectMoney } from "../../features/money/moneySlice";
-import { selectSeconds } from "../../features/seconds/secondsSlice";
 import { SecondsCounter } from "../SecondsCounter/SecondsCounter";
 import styles from "./Navbar.module.scss";
 export const Navbar = () => {
   const money = useAppSelector(selectMoney);
-  const seconds = useAppSelector(selectSeconds);
 
   return (
     <div className={styles.navbar}>
