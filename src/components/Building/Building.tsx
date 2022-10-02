a39db5243634cf2da802c9a1fb1a8ea412453dc4
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-02 15:22:09 +0200

    Updated building card

--- src/components/Building/Building.tsx
+++ src/components/Building/Building.tsx
@@ -1,18 +1,22 @@
-import { Card } from "antd";
+import { Button, Card } from "antd";
 import styles from "./Building.module.scss";
 export const Building = ({
   title,
   count,
+  handleBuy,
 }: {
   title: string;
   count: number;
+  handleBuy: () => void;
 }) => {
   return (
-    <div>
+    <div style={{ height: "100%" }}>
       <Card className={styles.buildingCard} title={title}>
         <div>Logo</div>
         <span>Owned : {count}</span>
-        <div>Buttons</div>
+        <div>
+          <Button onClick={handleBuy}>Buy</Button>
+        </div>
       </Card>
     </div>
   );
