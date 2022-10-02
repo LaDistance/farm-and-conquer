fcbc102be72a67624bbdaa3903311df4e0998399
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-02 16:28:55 +0200

    Better Building card

--- src/components/Building/Building.tsx
+++ src/components/Building/Building.tsx
@@ -3,19 +3,32 @@ import styles from "./Building.module.scss";
 export const Building = ({
   title,
   count,
+  price,
+  individualIncome,
+  income,
+  disabled,
   handleBuy,
 }: {
   title: string;
   count: number;
+  price: number;
+  individualIncome: number;
+  income: number;
+  disabled: boolean;
   handleBuy: () => void;
 }) => {
   return (
     <div style={{ height: "100%" }}>
       <Card className={styles.buildingCard} title={title}>
-        <div>Logo</div>
-        <span>Owned : {count}</span>
-        <div>
-          <Button onClick={handleBuy}>Buy</Button>
+        <div className={styles.info}>
+          <span>Owned : {count}</span>
+          <span>Income per building : {individualIncome} </span>
+          <span>Total income per turn : {income} </span>
+        </div>
+        <div className={styles.buttons}>
+          <Button disabled={disabled} onClick={handleBuy}>
+            Buy for {price} pognons !
+          </Button>
         </div>
       </Card>
     </div>
