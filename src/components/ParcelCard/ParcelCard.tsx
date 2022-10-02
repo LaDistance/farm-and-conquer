f683e8089854085f5712f2cc0303aca1815358fb
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-02 19:48:58 +0200

    Better owned parcel card

--- src/components/ParcelCard/ParcelCard.tsx
+++ src/components/ParcelCard/ParcelCard.tsx
@@ -1,14 +1,17 @@
 import { Button, Card } from "antd";
 import { Link } from "react-router-dom";
 import { Parcel } from "../../types/Parcel";
-
+import styles from "./ParcelCard.module.scss";
+import cardStyles from "../../styles/CardStyles.module.scss";
 export const ParcelCard = ({ parcel }: { parcel: Parcel }) => {
   return (
-    <Card title={`Parcel ${parcel.id}`}>
-      {" "}
-      <Link to={`/parcel/${parcel.id}`}>
-        <Button>Manage this parcel</Button>
-      </Link>
-    </Card>
+    <div style={{ height: "100%" }}>
+      <Card className={cardStyles.card} title={`Parcel ${parcel.id}`}>
+        {" "}
+        <Link to={`/parcel/${parcel.id}`}>
+          <Button type="primary">Manage this parcel</Button>
+        </Link>
+      </Card>
+    </div>
   );
 };
