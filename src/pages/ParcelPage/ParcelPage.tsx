75ccf4da5a86af939a83724c1cdd489b6fcb6b59
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-02 15:23:18 +0200

    Style of parcel page

--- src/pages/ParcelPage/ParcelPage.tsx
+++ src/pages/ParcelPage/ParcelPage.tsx
@@ -1,21 +1,37 @@
-import { Button } from "antd";
 import { useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import {
-  selectParcelFarmBuildings,
   selectParcels,
-  setParcelFarmBuildings,
+  setBuildingCount,
 } from "../../features/parcels/parcelsSlice";
-import { farmBuildings } from "../../data/farmBuildings";
 import { decrementMoney, selectMoney } from "../../features/money/moneySlice";
 import { Building } from "../../components/Building/Building";
+import { Responsive, WidthProvider } from "react-grid-layout";
 
+import { useEffect, useMemo } from "react";
+import { breakpoints, columns, getLayouts } from "../../data/layouts";
+
+// CSS
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import styles from "./ParcelPage.module.scss";
 export const ParcelPage = () => {
+  // Hooks
   const { parcelId } = useParams<{ parcelId: string }>();
   const parcels = useAppSelector(selectParcels);
   const money = useAppSelector(selectMoney);
-
   const dispatch = useAppDispatch();
+
+  // Grid
+  const ResponsiveReactGridLayout = useMemo(
+    () => WidthProvider(Responsive),
+    []
+  );
+  useEffect(() => {
+    console.log("Re-render parcel page");
+  }, []);
+
+  // Conditional rendering of the page
   if (!parcelId) {
     return <div>Invalid parcel id</div>;
   }
@@ -27,6 +43,7 @@ export const ParcelPage = () => {
     return <div>This parcel does not exist.</div>;
   }
 
+  // Functions
   const buyBuilding = (id: number, currentBalance: number) => {
     const farmBuildings = parcel.farmBuildings;
     const building = farmBuildings.find(
@@ -39,15 +56,10 @@ export const ParcelPage = () => {
 
       // Add the building to the parcel
       dispatch(
-        setParcelFarmBuildings({
-          id: parcel.id,
-          farmBuildings: [
-            ...farmBuildings,
-            {
-              building: building.building,
-              count: building.count + 1,
-            },
-          ],
+        setBuildingCount({
+          parcelId: parcel.id,
+          buildingId: id,
+          count: building.count + 1,
         })
       );
     } else {
@@ -57,14 +69,34 @@ export const ParcelPage = () => {
 
   return (
     <div>
-      <span>Parcel n°{parcel.id}</span>
-      <span>The Parcel UI will be here.</span>
-      {parcel.farmBuildings.map((building) => (
-        <Building title={building.building.name} count={building.count} />
-      ))}
-      <Button type="primary" onClick={() => buyBuilding(1, money)}>
-        Buy a small farm
-      </Button>
+      <ResponsiveReactGridLayout
+        verticalCompact={true}
+        layouts={getLayouts(parcel.farmBuildings)}
+        breakpoints={breakpoints}
+        preventCollision={false}
+        cols={columns}
+        rowHeight={200}
+        autoSize={true}
+        isDraggable={false}
+        isResizable={false}
+        margin={{
+          lg: [20, 20],
+          md: [20, 20],
+          sm: [20, 20],
+          xs: [20, 20],
+          xxs: [20, 20],
+        }}
+      >
+        {parcel.farmBuildings.map((building) => (
+          <div className={styles.gridItem} key={building.building.id}>
+            <Building
+              title={building.building.name}
+              count={building.count}
+              handleBuy={() => buyBuilding(building.building.id, money)}
+            />
+          </div>
+        ))}
+      </ResponsiveReactGridLayout>
     </div>
   );
 };
