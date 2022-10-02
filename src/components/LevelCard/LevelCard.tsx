a0fa9edf8926d381eb802599852842f632094819
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-03 00:55:30 +0200

    levels

--- src/components/LevelCard/LevelCard.tsx
+++ src/components/LevelCard/LevelCard.tsx
@@ -9,7 +9,15 @@ export const LevelCard = ({
   handleChooseLevel: (level: number) => void;
 }) => {
   return (
-    <Card title={level.name} className={styles.levelCard}>
+    <Card
+      title={
+        <div className={styles.header}>
+          <span className={styles.title}>{level.name}</span>
+          <span className={styles.description}>{level.description}</span>
+        </div>
+      }
+      className={styles.levelCard}
+    >
       <div className={styles.levelCardContent}>
         <Button type="primary" onClick={() => handleChooseLevel(level.id)}>
           Play this level
