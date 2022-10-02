704d3a07cc5a123d45b70214928ed7cb2fc3c97c
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-10-02 15:21:52 +0200

    Updated parcel card

--- src/components/Parcel.tsx
+++ src/components/Parcel.tsx
@@ -1,6 +1,14 @@
-import { Card } from "antd";
+import { Button, Card } from "antd";
+import { Link } from "react-router-dom";
 import { Parcel } from "../types/Parcel";
 
 export const ParcelCard = ({ parcel }: { parcel: Parcel }) => {
-  return <Card title={`Parcel ${parcel.id}`}></Card>;
+  return (
+    <Card title={`Parcel ${parcel.id}`}>
+      {" "}
+      <Link to={`/parcel/${parcel.id}`}>
+        <Button>Manage this parcel</Button>
+      </Link>
+    </Card>
+  );
 };
